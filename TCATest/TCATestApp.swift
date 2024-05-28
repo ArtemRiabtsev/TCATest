bd65bcf385f0ae36ef901e72c6b6bceac2c6f6d7
Artem <riabchik2013@gmail.com>
2024-05-28 14:00:44 +0300

    Initial Commit

---
+++ TCATest/TCATestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATestApp.swift
+//  TCATest
+//
+//  Created by Artem on 28.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TCATestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
